419fc6bad7a748eae988512a47b32d643a5961ee
Marcelo Mourier <marcelo_mourier@yahoo.com>
2025-04-17 14:48:30 -0600

    Added bash script to fix the snmptrapd issue running under Ubuntu 22.04/24.04.

---
+++ fix-ubuntu-snmptrapd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Stop the snmptrapd service
+sudo systemctl stop snmptrapd
+
+# Get rid of the snmptrapd.socket stuff
+sudo rm -f /etc/systemd/system/sockets.target.wants/snmptrapd.socket
+sudo mv /usr/lib/systemd/system/snmptrapd.socket /usr/lib/systemd/system/snmptrapd.socket.ORIG
+sudo rm -f /var/lib/systemd/deb-systemd-helper-enabled/sockets.target.wants/snmptrapd.socket
+
+# Create a basic snmptrapd.service file that
+# logs its messages to syslog.
+sudo cp snmptrapd.service /etc/systemd/system/snmptrapd.service
+
+# Reload the systemd daemon info
+sudo systemctl daemon-reload
+
+# Start the snmptrapd service
+sudo systemctl start snmptrapd
+
+# If you want the snmptrapd service to start
+# automatically when the system boots, enable
+# it...
+sudo systemctl enable snmptrapd
+
